49f7346e0052d15b9dd23147b5f33b97a88fa111
juancruzbidegain <53493827+juancruzbidegain@users.noreply.github.com>
2020-10-21 16:35:49 -0300

    Add files via upload

---
+++ passwordgeneretor.py
@@ -0,0 +1,28 @@
+import random
+
+
+def password_generetor():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    charts = MAYUS + MINUS + NUMS + CHARS
+    password = []
+
+    for i in  range(15):
+        caracater_random = random.choice(charts)
+        password.append(caracater_random)
+
+    password = ''.join(password)
+    return password
+
+def run():
+    password = password_generetor()
+    print(f'Tu nueva contraseña es: {password}')
+
+
+
+
+if __name__ == '__main__':
+    run()
